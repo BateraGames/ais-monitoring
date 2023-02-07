416a6a2a43487bbc4815bc897b458f070a9c305a
BateraGames <BateraGames180@gmail.com>
2023-02-07 15:02:06 +0700

    Livemap side info

--- src/Components/Livemap/livemap.js
+++ src/Components/Livemap/livemap.js
@@ -6,6 +6,7 @@ import { TileLayer } from 'react-leaflet/TileLayer'
 import { useMap } from 'react-leaflet/hooks'
 import RedMarker from '../../Assets/MarkerRed.png'
 import YellowMarker from '../../Assets/MarkerYellow.png'
+import Accordion from 'react-bootstrap/Accordion';
 import L from 'leaflet'
 
 const { BaseLayer, Overlay } = LayersControl;
@@ -73,13 +74,14 @@ function LiveMap({startPos, startZoomLev}){
               </LayersControl.BaseLayer>
 
               <LayersControl.Overlay name="Marker with popup" checked={true}>
-                <Marker position={position} icon={redMarker}>
+                <Marker position={position} icon={redMarker} onMouseOver={() => console.log('hover in')} onMouseOut={() => console.log('hover out')}>
                   <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
                 </Marker>
               </LayersControl.Overlay>
             </LayersControl>
           </MapContainer>
         </div>
+
         <div style={{position: 'absolute', bottom: '2vh', left: '2vh', width: '20vw', zIndex: '10' }}>
             <div style={{display: 'flex', flexDirection: 'column', gap: 5}}>
                 <div className="card">
@@ -103,18 +105,82 @@ function LiveMap({startPos, startZoomLev}){
                     </div>
                   </div>
                 </div>
-              <div style={{background: 'rgba(155, 155, 155, 0.7)', width: '100%', height: '3vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                    <div style={{display: 'flex', flexDirection: 'row', width: '90%', height: '100%', textAlign: 'left', gap: 5}}>
-                      <div style={{display: 'flex', flexDirection: 'row', width: '50%', textAlign: 'left'}}>
-                        Lat : {mousePos[0]}
-                      </div>
-                      <div style={{display: 'flex', flexDirection: 'row', width: '50%', textAlign: 'left'}}>
-                        Long : {mousePos[1]}
-                      </div>
-                    </div>
-                 
+              <div style={{background: 'rgba(155, 155, 155, 0.7)', width: '100%', height: '3vh', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: 5,}}>
+                <div style={{display: 'flex', flexDirection: 'row', width: '90%', height: '100%', textAlign: 'left', gap: 5}}>
+                  <div style={{display: 'flex', flexDirection: 'row', width: '50%', textAlign: 'left'}}>
+                    Lat : {mousePos[0]}
+                  </div>
+                  <div style={{display: 'flex', flexDirection: 'row', width: '50%', textAlign: 'left'}}>
+                    Long : {mousePos[1]}
+                  </div>
+                </div>
               </div>
             </div>
+        </div>
+
+        <div style={{background:'#ffffff', position: 'absolute', top: '1vh', right: '1vh', width: '20vw', height: '90vh', zIndex: '10', borderRadius: 5 }}>
+          <Accordion defaultActiveKey={['0', '1', '2']} alwaysOpen>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Ship Information</Accordion.Header>
+              <Accordion.Body>
+                <div style={{display: 'flex', flexDirection: 'row', gap: 5, width: '100%', textAlign: 'left'}}>
+                  <div className="card-text" style={{width: '50%'}}>Name</div>
+                  <div className="card-text" style={{width: '50%'}}>: KM Nama Kapal</div>
+                </div>
+                <div style={{display: 'flex', flexDirection: 'row', gap: 5, width: '100%', textAlign: 'left'}}>
+                  <div className="card-text" style={{width: '50%'}}>IMO / MMSI</div>
+                  <div className="card-text" style={{width: '50%'}}>: -/25413</div>
+                </div>
+                <div style={{display: 'flex', flexDirection: 'row', gap: 5, width: '100%', textAlign: 'left'}}>
+                  <div className="card-text" style={{width: '50%'}}>Lat</div>
+                  <div className="card-text" style={{width: '50%'}}>: 0.209</div>
+                </div>
+                <div style={{display: 'flex', flexDirection: 'row', gap: 5, width: '100%', textAlign: 'left'}}>
+                  <div className="card-text" style={{width: '50%'}}>Long</div>
+                  <div className="card-text" style={{width: '50%'}}>: 110.2</div>
+                </div>
+                <div style={{display: 'flex', flexDirection: 'row', gap: 5, width: '100%', textAlign: 'left'}}>
+                  <div className="card-text" style={{width: '50%'}}>Type</div>
+                  <div className="card-text" style={{width: '50%'}}>: Cargo</div>
+                </div>
+                <div style={{display: 'flex', flexDirection: 'row', gap: 5, width: '100%', textAlign: 'left'}}>
+                  <div className="card-text" style={{width: '50%'}}>Flag</div>
+                  <div className="card-text" style={{width: '50%'}}>: Indonesia</div>
+                </div>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Voyage Information</Accordion.Header>
+              <Accordion.Body>
+                <div style={{display: 'flex', flexDirection: 'row', gap: 5, width: '100%', textAlign: 'left'}}>
+                  <div className="card-text" style={{width: '50%'}}>Origin</div>
+                  <div className="card-text" style={{width: '50%'}}>: T. Perak Surabaya</div>
+                </div>
+                <div style={{display: 'flex', flexDirection: 'row', gap: 5, width: '100%', textAlign: 'left'}}>
+                  <div className="card-text" style={{width: '50%'}}>Destination</div>
+                  <div className="card-text" style={{width: '50%'}}>: T. Priok Jakarta</div>
+                </div>
+                <div style={{display: 'flex', flexDirection: 'row', gap: 5, width: '100%', textAlign: 'left'}}>
+                  <div className="card-text" style={{width: '50%'}}>ETA</div>
+                  <div className="card-text" style={{width: '50%'}}>: 03 Feb 23, 19.00</div>
+                </div>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Nearest Ships</Accordion.Header>
+              <Accordion.Body>
+                <div style={{display: 'flex', flexDirection: 'column', gap: 5, width: '100%', textAlign: 'left'}}>
+                  <div>1. Kapal Utama 1</div>
+                  <div>2. Kapal Lain 1</div>
+                  <div>3. Kapal Lain 2</div>
+                  <div>4. Kapal Lain 3</div>
+                  <div>5. Kapal Lain 4</div>
+                  <div>6. Kapal Lain 5</div>
+                  <div>7. Kapal Lain 6</div>
+                </div>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </div>
       </div>
   );
