eec8f00be97836d5e7f98fe286805012c2a2d8e6
Dimasariza <roganda.sihombing11@gmail.com>
2023-02-08 20:06:42 +0700

    Updates

---
+++ main.js
@@ -0,0 +1,66 @@
+const { app, BrowserWindow, ipcMain, Menu } = require('electron')
+require('@electron/remote/main').initialize()
+
+const path = require('path')
+// const isDev = require('electron-is-dev')
+  
+let win
+
+function createWindow(){
+    win = new BrowserWindow({
+        width: 1024,
+        height: 600,
+        fullscreen: true,
+        autoHideMenuBar: true,
+        frame:false,
+        webPrefrences: {
+            nodeIntegration: true,
+        }
+    })
+
+   
+    win.webContents.openDevTools();
+
+    // win.loadURL(
+    //     isDev
+    //     ? 'http://localhost:3000'
+    //     : `file://${path.join(__dirname, '../build/index.html')}`
+    // )
+
+    // console.log(path)
+    // win.loadURL("http://localhost:3000");
+    console.log(`file://${path.join(__dirname, '../build/index.html')}`);
+    win.loadURL(
+        `file://${path.join(__dirname, '../build/index.html')}`
+    )
+
+    win.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
+    })
+
+    // win.webContents.openDevTools()
+}
+
+// app.whenReady().then(createWindow)
+
+app.on('ready', function(){
+    // ipcMain.on('counter-value', (_event, value) => {
+    //     console.log(value) // will print value to Node console
+    // })
+    createWindow();
+});
+
+app.on('window-all-closed', function(){
+    if(process.platform !== 'darwin'){
+        app.quit()
+    }
+})
+
+app.on('activate', function(){
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
