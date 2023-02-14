76d4bcdeafb78c7b768984015c998c35ae2d7c02
BateraGames <BateraGames180@gmail.com>
2023-02-14 13:37:28 +0700

    Add more ship in database

--- src/Components/DataStructure/shipDatabase.js
+++ src/Components/DataStructure/shipDatabase.js
@@ -81,6 +81,30 @@ export default class ShipDatabase extends EventEmitter{
                         ),
                         [] 
                     ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 6', '~/25413', 
+                            -6.969278, 112.708546,
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 7', '~/25413', 
+                            -7.464009, 112.882901,
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
 
                 ] 
             ),
@@ -155,7 +179,321 @@ export default class ShipDatabase extends EventEmitter{
                         ),
                         [] 
                     ),
-
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 6', '~/25413', 
+                            -4.879693, 120.617338, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 7', '~/25413', 
+                            -4.756490, 120.724422,
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                ] 
+            ),
+            new MainShipData(
+                new ShipInfo(
+                    'Main Ship 3', '~/25413', 
+                    2.893220, 106.262427, 
+                    "Cargo", 'Indonesia', 
+                    39941, 50476
+                ),
+                new VoyageInfo(
+                    "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                ),
+                [
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 1', '~/25413', 
+                            3.254004, 105.518328, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 2', '~/25413', 
+                            3.477305, 106.024775, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 3', '~/25413', 
+                            3.488275, 106.236198, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 4', '~/25413', 
+                             2.500268, 105.669268, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 5', '~/25413', 
+                            2.676180, 106.093419, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 6', '~/25413', 
+                            2.925910, 105.975352, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 7', '~/25413', 
+                            2.684166, 106.498485,
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                ] 
+            ),
+            new MainShipData(
+                new ShipInfo(
+                    'Main Ship 4', '~/25413', 
+                    -4.544210, 107.222997, 
+                    "Cargo", 'Indonesia', 
+                    39941, 50476
+                ),
+                new VoyageInfo(
+                    "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                ),
+                [
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 1', '~/25413', 
+                            -4.201810, 107.014910, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 2', '~/25413', 
+                            -4.197700, 107.595636, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 3', '~/25413', 
+                            -4.689403, 107.623094, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 4', '~/25413', 
+                            -4.835890, 107.414417, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 5', '~/25413', 
+                            -4.798929, 107.049232, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 6', '~/25413', 
+                            -4.605878, 106.896842, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 7', '~/25413', 
+                            -4.549733, 107.112384,
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                ] 
+            ),
+            new MainShipData(
+                new ShipInfo(
+                    'Main Ship 5', '~/25413', 
+                    -7.516674, 124.048975, 
+                    "Cargo", 'Indonesia', 
+                    39941, 50476
+                ),
+                new VoyageInfo(
+                    "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                ),
+                [
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 1', '~/25413', 
+                            -7.864186, 123.504116, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 2', '~/25413', 
+                            -7.647753, 123.689455, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 3', '~/25413', 
+                            -8.070984, 124.537893, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 4', '~/25413', 
+                            -8.087306, 124.024437, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 5', '~/25413', 
+                            -7.640945, 123.992860, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 6', '~/25413', 
+                            -7.324943, 124.027182, 
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
+                    new ShipData(
+                        new ShipInfo(
+                            'Other Ship 7', '~/25413', 
+                            -7.632775, 124.233114,
+                            "Cargo", 'Indonesia', 
+                            39941, 50476
+                        ),
+                        new VoyageInfo(
+                            "T. Perak Surabaya", "T. Priok Jakarta", "03 Feb 23, 19.00"
+                        ),
+                        [] 
+                    ),
                 ] 
             )
         ]
